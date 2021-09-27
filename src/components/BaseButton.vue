a1a0ff3440f808f31e1111f98cd9bc3f030d8186
Laura Rose <straxis@protonmail.com>
2021-09-27 17:13:56 -0400

    Add BaseButton

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="block max-w-sm px-4 py-2 bg-burntorange hover:bg-darkorange rounded-lg disabled:opacity-50 disabled:cursor-not-allowed border-darkorange">{{title}}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'base-button',
+  props: {
+    title: String
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
