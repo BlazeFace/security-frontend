5f5993cdeafb3cd6d899c0a84f4fcbced1799a3f
Laura Rose <straxis@protonmail.com>
2021-09-28 13:24:04 -0400

    Add mock topic data for testing

--- src/views/Topics.vue
+++ src/views/Topics.vue
@@ -1,4 +1,30 @@
-<script>
+<script lang="ts">
+  const topicPages = [
+    {
+      title: "Title",
+      subheading: "Subheading",
+      content: "This is the page content.",
+      concerns: [
+        "Privacy",
+        "Data Management"
+      ],
+      devices: [
+        "iPhone"
+      ],
+      nextSteps: {
+        name: "Your Next Steps",
+        description: "Description"
+      },
+      importance: {
+        name: "Why is This Important?",
+        description: "Description"
+      },
+      learnMore: {
+        name: "Learn More",
+        description: "Description"
+      }
+    }
+  ]
 </script>
 
 <template>
