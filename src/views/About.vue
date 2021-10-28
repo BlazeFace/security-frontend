167f719e294a262b79587f9ed49b9a25f3d5ef52
Laura Rose <straxis@protonmail.com>
2021-10-28 07:35:11 -0400

    Add Crystal's about page and the data vis team's report

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <h1>About Us</h1>
+  <div class="h-auto p-2 bg-white lg:px-10">
+    <div class="p-4 row-start-2  md:row-start-1">
+      <div class="p-4 bg-maroon rounded-2xl text-white">
+        <h1 class="font-bold p-2 text-center">Who We Are</h1>
+        <div class="text-left p-1">
+          <p class="py-2">We are students in Dr. Aaron Brantley's BIT 4164 Future of Security Capstone course, and we have backgrounds in a variety of technical and non-technical majors.</p>
+          <p class="py-2"></p>
+        </div>
+        <h1 class="font-bold p-2 text-center">Our Digital Security Planner Project</h1>
+        <div class="text-left p-1">
+          <p class="py-2">Utilizing our wide range of academic specialties, we as a class have worked together to create a digital security planner that Virginia Tech students could use to strengthen their personal and digital security. Our planner consists of a variety of topics that may be of your interest, and if you are unsure of what areas you wish to secure further, there is a quiz provided on a separate tab that will prompt you answer a variety of questions that will help filter out the topics so that a selected handful are chosen for you based on your options.</p>
+          <p class="py-2"></p>
+        </div>
+        <h1 class="font-bold p-2 text-center">Future of the Planner</h1>
+        <div class="text-left p-1">
+          <p class="py-2">Although we have only created a beta version of the planner this semester, we hope that the next steps to advancing the planner will be to create a more appealing website with a fully interactive quiz and an increased list of security suggestion topics.</p>
+          <p class="py-2"></p>
+        </div>
+        <p>Open a PDF file <a href="Cybersecurity_report.pdf" download>example</a>.</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +31,11 @@ export default {
 </script>
 
 <style scoped>
+h1 {
+  font-size: xx-large;
+}
 
+p {
+  font-size: large;
+}
 </style>
